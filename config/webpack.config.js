1d527b922375b4d44e14e9fda07dc3996a22eb29
victor hazbun <victorhazbun87@gmail.com>
2017-05-06 19:59:13 -0500

    Storing View Templates in HTML Files

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -29,10 +29,11 @@ var config = {
   module: {
     loaders: [
       { test: /\.css$/, loader: 'style-loader!css-loader' },
-      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
-      { test: /\.(woff|woff2)$/, loader:"url?prefix=font/&limit=5000" },
-      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
-      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" }
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
+      { test: /\.(woff|woff2)$/, loader:'url?prefix=font/&limit=5000' },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' },
+      {test: /\.html/, loader: 'raw'}
     ]
   },
 
--- spec/javascripts/CustomerSearchComponent.spec.js
+++ spec/javascripts/CustomerSearchComponent.spec.js
@@ -1,5 +1,12 @@
-var CustomerSearchComponent =
-      require("../../webpack/CustomerSearchComponent");
+var proxyquire = require("proxyquire");
+var CustomerSearchComponent = proxyquire(
+  "../../webpack/CustomerSearchComponent",
+  {
+    "./CustomerSearchComponent.html": {
+      "@noCallThru": "true"
+    }
+  }
+);
 var td = require("testdouble");
 
 var component = null;
